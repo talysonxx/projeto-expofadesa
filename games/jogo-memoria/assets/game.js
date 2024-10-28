e962198c505ae37d98ccf2c5258c998260cdeab4
Talyson André <talysonoficial12@gmail.com>
2024-10-28 20:23:59 -0300

    sera

--- games/jogo-memoria/assets/game.js
+++ games/jogo-memoria/assets/game.js
@@ -4,62 +4,71 @@ import {
   verifyUser
 } from "../../../scripts/firebase.js";
 
-
-
-
-
 const profilePIC = document.getElementById("profilePIC");
 
 var logged = await verifyUser();
 if (logged) {
   profilePIC.src = auth.currentUser.photoURL;
-
-
 }
-
-
-
 
 let game = {
   lockMode: false,
   firstCard: null,
   secondCard: null,
-  setCard: function(id) {
-    let card = this.cards.filter(card => card.id == id)[0] 
+  timerStarted: false, // flag para verificar se o tempo começou
+  startTime: null, // variável para armazenar o início do tempo
+  setCard: function (id) {
+    let card = this.cards.filter(card => card.id == id)[0];
 
-    if (card.flipped || this.lockMode) return false
+    if (card.flipped || this.lockMode) return false;
+
     if (!this.firstCard) {
-      this.firstCard = card
-      this.firstCard.flipped = true
+      this.firstCard = card;
+      this.firstCard.flipped = true;
 
-      return true
+      // Inicia o cronômetro quando a primeira carta é virada
+      if (!this.timerStarted) {
+        this.startTimer();
+        this.timerStarted = true;
+      }
+
+      return true;
     } else {
-      this.secondCard = card
-      this.secondCard.flipped = true
-      this.lockMode = true
+      this.secondCard = card;
+      this.secondCard.flipped = true;
+      this.lockMode = true;
 
-      return true
+      return true;
     }
   },
-  checkMath: function() {
-    // if (!this.firstCard || !this.secondCard) return false
-
-    let check = this.firstCard.icon === this.secondCard.icon
-    return check
+  startTimer: function () {
+    this.startTime = Date.now(); // armazena o tempo de início
+    console.log("O cronômetro começou!");
   },
-  clearCards: function() {
-    this.firstCard = null
-    this.secondCard = null
-    this.lockMode = false
+  stopTimer: function () {
+    let elapsedTime = Date.now() - this.startTime; // calcula o tempo decorrido
+    console.log(`O tempo decorrido foi: ${elapsedTime / 1000} segundos`);
   },
-  unflipCards() {
-    this.firstCard.flipped = false
-    this.secondCard.flipped = false
-    this.clearCards()
+  checkMath: function () {
+    let check = this.firstCard.icon === this.secondCard.icon;
+    return check;
   },
-  checkGameOver() {
-    let gameOver = this.cards.filter(card => !card.flipped).length == 0
-    return gameOver
+  clearCards: function () {
+    this.firstCard = null;
+    this.secondCard = null;
+    this.lockMode = false;
+  },
+  unflipCards: function () {
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
+  },
+  checkGameOver: function () {
+    let gameOver = this.cards.filter(card => !card.flipped).length == 0;
+    if (gameOver) {
+      this.stopTimer(); // Para o cronômetro quando o jogo termina
+    }
+    return gameOver;
   },
   techs: [
     'css',
@@ -72,20 +81,19 @@ let game = {
     'reactjs',
     'ubuntu',
     'vscode'
-    ],
+  ],
   cards: null,
-  createCardsFromTechs: function() {
-    this.cards = []
-  
+  createCardsFromTechs: function () {
+    this.cards = [];
+
     this.techs.map(tech => {
-        this.cards.push(this.createPairFromTech(tech))
-      })
-  
-      // if each pair of cards is an array, fletmap breaks it apart
-      this.cards = this.cards.flatMap(pair => pair)
-      this.shuffleCards()
+      this.cards.push(this.createPairFromTech(tech));
+    });
+
+    this.cards = this.cards.flatMap(pair => pair);
+    this.shuffleCards();
   },
-  createPairFromTech: function(tech) {
+  createPairFromTech: function (tech) {
     return [{
       id: this.createIdWithTech(tech),
       icon: tech,
@@ -94,22 +102,22 @@ let game = {
       id: this.createIdWithTech(tech),
       icon: tech,
       flipped: false
-    }]
+    }];
   },
-  createIdWithTech: function(tech) {
-    return tech + parseInt(Math.random() * 1000)
+  createIdWithTech: function (tech) {
+    return tech + parseInt(Math.random() * 1000);
   },
-  shuffleCards: function() {
-    let currentIndex = this.cards.length
-    let randomIndex = 0
-  
+  shuffleCards: function () {
+    let currentIndex = this.cards.length;
+    let randomIndex = 0;
+
     while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex--
-  
-      [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
     }
   }
-}
+};
 
 export { game };
